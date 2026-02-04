1fc34e083eba5159f089917cfaf2b148f87533cf
natillita10 <napologo1010@gmail.com>
2026-02-04 16:55:12 +0000

    Solicite datos al usuario sobre sus prestamos

--- BibliotecaApp.java
+++ BibliotecaApp.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,7 +7,7 @@ public class BibliotecaApp {
     // prestamo = [idPrestamo, nombreUsuario, tituloLibro, diasPrestamo, multaPorDia]
     static ArrayList<ArrayList<Object>> prestamos = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
-    String prestamos[][]= new int [5][200];
+    static String Prestamos[][] = new String[5][200];
 
     public static void main(String[] args) {
         int opcion;
@@ -15,14 +16,22 @@ public class BibliotecaApp {
             opcion = leerEntero("Seleccione una opción: ");
 
             switch (opcion) {
-                case 1 -> registrarPrestamo();
-                case 2 -> mostrarPrestamos();
-                case 3 -> buscarPrestamoPorId();
-                case 4 -> actualizarPrestamo();
-                case 5 -> eliminarPrestamo();
-                case 6 -> calcularTotalMultas();
-                case 7 -> System.out.println("Saliendo...");
-                default -> System.out.println("Opción inválida.");
+                case 1 ->
+                    registrarPrestamo();
+                case 2 ->
+                    mostrarPrestamos();
+                case 3 ->
+                    buscarPrestamoPorId();
+                case 4 ->
+                    actualizarPrestamo();
+                case 5 ->
+                    eliminarPrestamo();
+                case 6 ->
+                    calcularTotalMultas();
+                case 7 ->
+                    System.out.println("Saliendo...");
+                default ->
+                    System.out.println("Opción inválida.");
             }
             System.out.println();
         } while (opcion != 7);
@@ -42,18 +51,58 @@ public class BibliotecaApp {
     }
 
     // ====== CRUD (por implementar) ======
-    static void registrarPrestamo() { /* TODO */ }
-    static void mostrarPrestamos() { /* TODO */ }
-    static void buscarPrestamoPorId() { /* TODO */ }
-    static void actualizarPrestamo() { /* TODO */ }
-    static void eliminarPrestamo() { /* TODO */ }
+    static void registrarPrestamo() {
+        for (int i = 0; i < 200; i++) {
+            System.out.println("Ingrese su Identificación");
+            String id = sc.nextLine();
+            Prestamos[0][i] = id;
+            System.out.println("Ingrese su Nombre de Usuario");
+            String nu = sc.nextLine();
+            Prestamos[1][i] = nu;
+            System.out.println("Ingrese el Titulo del Libro");
+            String tl = sc.nextLine();
+            Prestamos[2][i] = tl;
+            System.out.println("Ingrese los días de prestamos ");
+            String dp = sc.nextLine();
+            Prestamos[3][i] = dp;
+            System.out.println("Ingrese la multa por dia");
+            String md = sc.nextLine();
+            Prestamos[4][i] = md;
+        }
+
+    }
+
+    static void mostrarPrestamos() {
+        System.out.print("ID");
+        System.out.print("Nombre de Usuario:");
+        System.out.print("ID");
+        System.out.print("ID");
+        System.out.print("ID");
+
+        for (int i = 0; i < 200; i++) {
+            System.out.print(Prestamos[0][i]);
+            System.out.print(Prestamos[1][i]);
+            System.out.print(Prestamos[2][i]);
+            System.out.print(Prestamos[3][i]);
+            System.out.print(Prestamos[4][i]);
+            System.out.print("\n");
+        }
+
+    }
+
+    static void buscarPrestamoPorId() {
+        /* TODO */ }
+
+    static void actualizarPrestamo() {
+        /* TODO */ }
+
+    static void eliminarPrestamo() {
+        /* TODO */ }
 
     // ====== Cálculo (por implementar) ======
-    static void calcularTotalMultas() { 
-        
-        
+    static void calcularTotalMultas() {
 
-     }
+    }
 
     // ====== Utilidades mínimas ======
     static int leerEntero(String msg) {
