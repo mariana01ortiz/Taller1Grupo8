b034a59fd9cec9c3787c0da07867fb1e9d78e8dc
mariana01ortiz <ortizmariana@uninorte.edu.co>
2026-02-09 13:57:41 +0000

    Se completo y corrigio el item 4

--- BibliotecaApp.java
+++ BibliotecaApp.java
@@ -47,27 +47,38 @@ public class BibliotecaApp {
     static void buscarPrestamoPorId() { 
         System.out.println("Ingrese el ID a buscar:")
         int IDbuscar;
+        Boolean encontrado = false;
+        IDbuscar = leer.nextInt();
         while (int j < 200){
             if (IDbuscar = prestamos [1][i]){
-                System.out.println("ID del usuario: " + prestamos [1][i])
-                System.out.println("Nombre del usuario: " + prestamos [2][i])
-                System.out.println("Libro: " + prestamos [3][i])
-                System.out.println("Dias de prestamo: " + prestamos [4][i])
-                System.out.println("Multa por dia: " + prestamos [5][i])
+                System.out.println("ID del usuario: " + prestamos [1][i]);
+                System.out.println("Nombre del usuario: " + prestamos [2][i]);
+                System.out.println("Libro: " + prestamos [3][i]);
+                System.out.println("Dias de prestamo: " + prestamos [4][i]);
+                System.out.println("Multa por dia: " + prestamos [5][i]);
+                encontrado = true;
             }else{
                 i = i+1;
             }
+            if (encontrado = false){
+            System.out.println ("no se encontro usuario con ese id")
         }
      }
     static void actualizarPrestamo() { 
         System.out.println ("Ingrese el numero del prestamo que desea cambiar: ")
         int NumeroU;
-        System.out.println ("Ingrese el numero de el dato que desea cambiar: ")
-        System.out.println ("1 = Id del usuario")
-        System.out.println ("2 = Nombre de usuario")
-        System.out.println ("3 = Libro")
-        System.out.println ("4 = Dias de prestamo")
+        NumeroU = leer.nextInt();
         
+                System.out.println("Ingrese El nuevo ID:")
+                prestamos [1][i]= sc.nextLine;
+                System.out.println("Ingrese El nuevo Nombre:")
+                prestamos [2][i]= sc.nextLine;
+                System.out.println("Ingrese El nuevo título del Libro:")
+                prestamos [3][i]= sc.nextLine;
+                System.out.println("Ingrese Los días de prestamo:")
+                prestamos [4][i]= sc.nextLine;
+                System.out.println("Ingrese La multa por día:")
+                prestamos [5][i]= sc.nextLine;
         }
     static void eliminarPrestamo() { /* TODO */ }
 
